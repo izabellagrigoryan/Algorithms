bfa37ccf72bc824d8468351566060304a6cd9323
izabellagrigoryan <bellagrigoryanmail@gmail.com>
2024-02-25 09:58:16 +0400

    commit of the #561 solution

--- LeetCode Solutions/#561.cpp
+++ LeetCode Solutions/#561.cpp
@@ -2,19 +2,24 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
+class Solution{
+public:
+    int arrayPairSum(std::vector<int>&nums) {
+        std::sort(nums.begin(), nums.end());
+
+        int sum = 0;
+        // Sum up the minimum of each pair
+        for (int i = 0; i < nums.size(); i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
